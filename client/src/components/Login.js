b9270e025441eacf242305a9abd795436661f492
Chaehwan Lim <chlim428@gmail.com>
2020-02-25 22:55:46 +0900

    Refactored and created login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,143 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Divider from '@material-ui/core/Divider';
+import { Grid, Card, TextField} from '@material-ui/core';
+import Fab from '@material-ui/core/Fab';
+
+
+const useStyles = makeStyles(theme => ({
+    background: {
+        transitionDuration : '0.8s',
+        paddingTop: '7rem',
+        paddingBottom: '3rem',
+    },
+    header: {
+        align: 'center',
+    },
+    title: {
+        paddingBottom: '2rem',
+        textAlign: 'left',
+        fontSize: '2.5rem',
+        fontWeight: '900',
+        letterSpacing: '0.1rem',
+    },
+    subtitle: {
+        fontSize: '2.2rem',
+        fontWeight: '700',
+        margin: '1.5rem',
+        textAlign: 'center',
+    },
+    card: {
+        marginTop: '2rem',
+        fontSize: '1.7rem',
+    },
+    login: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: '1.5rem',
+        marginBottom: '3rem',
+    },
+    loginInput: {
+        fontSize: '2rem',
+    },
+    btn: {
+        marginTop: '3rem',
+        fontSize:'2rem',
+        background: 'transparent',
+        color: 'white',
+        transition: '0.7s',
+        '&:hover': {
+            transform: 'scale(1.1)',
+            transition: '0.7s',
+        }
+    },
+    _divider:{
+        marginTop: '3rem',
+    },
+    loginDivider: {
+
+    },
+    inputFocused: {
+        color: 'black'
+    }
+}))
 
 export default function Login() {
+    const InputProps = { style: {fontSize: '2rem'}};
+    const InputLabelProps = { style: {fontSize: '1.7rem', color: 'black'} }
+
+    const classes = useStyles();
+    const primary = "#2196F3"
+
     return (
-        <Container maxWidth="lg">
-            <div>Login</div>
-        </Container>
+        <div className={classes.background}>
+            <Container maxWidth="lg" className={classes.Header}>
+                <div className={classes.title}>로그인</div>
+            </Container>
+            <Divider className={classes._divider}/>
+            <Container maxWidth="lg">
+                <Grid container spacing={10}>
+                    <Grid item xs={12} md={6}>
+                        <Card className={classes.card}>
+                        <div className={classes.subtitle}>
+                            이미 가입한 계정이 있으신가요?
+                        </div>
+                        <Divider className={classes.loginDivider}/>
+                        <form noValidate autoComplete="off" className={classes.login}>
+                            <TextField id="outlined-basic" label="이름"
+                            inputProps={InputProps}
+                            InputLabelProps={{ classes: {focused: classes.inputFocused}}}/><br />
+                            <TextField
+                                id="standard-password-input"
+                                label="패스워드"
+                                type="password"
+                                autoComplete="current-password"
+                                inputProps={InputProps}
+                                InputLabelProps={{ classes: {focused: classes.inputFocused}}}
+                            />
+                            <Fab variant="extended" className={classes.btn} 
+                            style={{background: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+                                    boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .3)',}}>
+                            로그인</Fab>
+                        </form>
+                        
+                        </Card>
+                        
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Card className={classes.card}>
+                        <div className={classes.subtitle}>
+                            <span style={{fontFamily: 'Product Sans'}}>PlayLab</span>에 처음 오셨나요?
+                        </div>
+                        <Divider className={classes.loginDivider}/>
+                        
+                        <form noValidate autoComplete="off" className={classes.login}>
+                            <TextField id="outlined-basic" label="이름"
+                            inputProps={InputProps}
+                            InputLabelProps={{ classes: {focused: classes.inputFocused}}}/><br />
+                            <TextField
+                                id="standard-password-input"
+                                label="패스워드"
+                                type="password"
+                                autoComplete="current-password"
+                                inputProps={InputProps}
+                                InputLabelProps={{ classes: {focused: classes.inputFocused}}}
+                            />
+                            <Fab variant="extended" className={classes.btn}
+                            style={{background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+                                    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',}}>
+                            회원가입</Fab>
+                        </form>
+                        
+                        </Card>
+                        
+                    </Grid>
+                </Grid>
+            </Container>
+            
+        </div>
+        
     )
 }

b9d8cb84d215595b11307a5dbdb159c3136fca95
Chaehwan Lim <chlim428@gmail.com>
2020-03-06 11:05:35 +0900

    rebuild 6

---
+++ client_build/precache-manifest.dedf1447df4330929f7f13a011e57de8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79769bb4f3d0f5c2a96d90f9f59c2ae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e1e330cb441d5e4e91",
+    "url": "/static/css/main.6733bea3.chunk.css"
+  },
+  {
+    "revision": "68683ba209f526929589",
+    "url": "/static/js/2.6715c1e1.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.6715c1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e1e330cb441d5e4e91",
+    "url": "/static/js/main.7c95e527.chunk.js"
+  },
+  {
+    "revision": "ca2b2bef3ed2be4d0067",
+    "url": "/static/js/runtime-main.28abf549.js"
+  },
+  {
+    "revision": "7c54f8a7b43684b552f3aad36bd782cd",
+    "url": "/static/media/background2.7c54f8a7.jpg"
+  },
+  {
+    "revision": "52e5256e206312d9ffd539565c3b8924",
+    "url": "/static/media/samsungsharpsans-bold.52e5256e.otf"
+  }
+]);
